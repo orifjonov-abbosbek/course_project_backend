495ce21673296b1008e0c3649260123c30efd354
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-09-13 04:20:59 +0500

    review completed basically

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -54,11 +54,59 @@ exports.getReviewByUserId = async (req, res) => {
       where: { userId: user.id },
     });
     res.json(reviews);
-
-
-    
   } catch (err) {
     console.error("error while fetching specified review", err);
     req.status(500).json({ error: "internal server error" });
   }
 };
+
+exports.updateRewview = async (req, res) => {
+  const reviewId = req.params.reviewId;
+  try {
+    const review = await Review.findByPk(reviewId);
+
+    if (!review) {
+      return res.status(404).json({ message: "review not found" });
+    }
+
+    const { reviewName, reviewedItem, group, tags, reviewedText, rating } =
+      req.body;
+
+    review.reviewName = reviewName;
+    review.reviewedItem = reviewedItem;
+    review.group = group;
+    review.tags = tags;
+    review.reviewedText = reviewedText;
+    review.rating = rating;
+
+    await review.save();
+
+    res.json(review);
+  } catch (err) {
+    console.log("error while updating review", err);
+    req.status(500).json({ message: "internal server error" });
+  }
+};
+
+
+
+exports.deleteReview = async (req, res ) => {
+  const reviewId = req.params.reviewId;
+
+  try{
+    const review = await Review.findByPk(reviewId);
+
+    if(!review){
+      return res.status(404).json({message: 'review not found '});
+
+    }
+ 
+    await review.destroy();
+
+    res.status(204).send()
+  }
+  catch(err){
+    console.error('error while deleting review', err);
+    req.status(500).json({message: 'internal server error'});
+  }
+}
